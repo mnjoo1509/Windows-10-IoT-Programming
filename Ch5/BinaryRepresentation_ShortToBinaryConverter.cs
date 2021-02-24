02eaa462ae1a99eb3ac895c525bbe05ee64e4460
mnjoo1509 <79436078+mnjoo1509@users.noreply.github.com>
2021-02-24 10:29:48 +0900

    Update BinaryRepresentation_ShortToBinaryConverter.cs
    ex5-5(5-5,6)

--- Ch5/BinaryRepresentation_ShortToBinaryConverter.cs
+++ Ch5/BinaryRepresentation_ShortToBinaryConverter.cs
@@ -21,50 +21,22 @@ namespace BinaryRepresentation
                 {
                     const int bitcount = 16;
 
-                    result = System.Convert.ToString(inputValue, 2);
-                    result = result.PadLeft(bitcount, '0');
+                    //result = System.Convert.ToString(inputValue, 2);
+                    //result = result.PadLeft(bitcount, '0');
+
+                    for (int i = 0; i < bitcount; i++)
+                    {
+                        result += IsBitSet(inputValue, i);
+                    }
                 }
             }
 
             return result;
         }
 
-        public object ConvertBack(object value, Type targetType,
-            object parameter, string language)
+        private string IsBitSet(short value, int position)
         {
-            throw new NotImplementedException();
-        }
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.UI.Xaml.Data;
-
-namespace BinaryRepresentation
-{
-    public class ShortToBinaryConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType,
-            object parameter, string language)
-        {
-            string result = string.Empty;
-
-            if (value != null)
-            {
-                short inputValue;
-                if (short.TryParse(value.ToString(), out inputValue))
-                {
-                    const int bitcount = 16;
-
-                    result = System.Convert.ToString(inputValue, 2);
-                    result = result.PadLeft(bitcount, '0');
-                }
-            }
-
-            return result;
+            return (value & (1 << position)) > 0 ? "1" : "0";
         }
 
         public object ConvertBack(object value, Type targetType,
